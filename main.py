1abdf790149f4c942ef92476833dba8f6d29647e
mtmattqq <124585214+mtmattqq@users.noreply.github.com>
2023-03-11 20:43:45 +0800

    Update

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import pygame
-
+import Element
 # pygame init
 pygame.init()
 screen=pygame.display.set_mode((1000,600))
 screen.fill((255,255,255))
 pygame.display.set_caption("結構式繪圖軟體")
-font=pygame.font.SysFont('microsoftjhenghei',50)
+font=pygame.font.SysFont('cambriamath',36)
 clock=pygame.time.Clock()
 # programIcon=pygame.image.load('icon.png')
 # pygame.display.set_icon(programIcon)
 
 # variables
 FPS=60
-elements=[]
-
+elements=[Element.Element()]
+relativePos=Element.vec2D(500,300)
 
 def show_text(text='',x=0,y=0,color=(0,0,0)):
     text=font.render(text,True,color)
     textRect=text.get_rect()
-    textRect.center=(x,y)
+    textRect.center=(x+relativePos.x,y+relativePos.y)
     screen.blit(text,textRect)
 
 # main loop
@@ -31,6 +31,10 @@ while InGame:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
+    
+    # show elements
+    for element in elements:
+        show_text(element.text,element.pos.x,element.pos.y)
     
     pygame.display.flip()
     clock.tick(FPS)
